66a51b850750695f5663eb6883e1b9f5fd1c2c33
Mar-Issah <masy370@gmail.com>
2023-12-18 14:47:46 -0600

    Feat: implement script that return top 10 of the most present IPs

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -1,28 +1,29 @@
 #!/usr/bin/env python3
-""" MongoDB Operations with Python using pymongo """
+""" 15. Log stats - new version """
 from pymongo import MongoClient
 
 if __name__ == "__main__":
     """ Provides some stats about Nginx logs stored in MongoDB """
     client = MongoClient('mongodb://127.0.0.1:27017')
-    nginx_collection = client.logs.nginx
+    collection = client.logs.nginx
 
-    n_logs = nginx_collection.count_documents({})
+    n_logs = collection.count_documents({})
     print(f'{n_logs} logs')
 
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+
     print('Methods:')
     for method in methods:
-        count = nginx_collection.count_documents({"method": method})
+        count = collection.count_documents({"method": method})
         print(f'\tmethod {method}: {count}')
 
-    status_check = nginx_collection.count_documents(
+    status_check = collection.count_documents(
         {"method": "GET", "path": "/status"}
     )
 
     print(f'{status_check} status check')
 
-    top_ips = nginx_collection.aggregate([
+    ips = collection.aggregate([
         {"$group":
             {
                 "_id": "$ip",
@@ -39,7 +40,7 @@ if __name__ == "__main__":
     ])
 
     print("IPs:")
-    for top_ip in top_ips:
-        ip = top_ip.get("ip")
-        count = top_ip.get("count")
+    for ip in ips:
+        ip = ip.get("ip")
+        count = ip.get("count")
         print(f'\t{ip}: {count}')
