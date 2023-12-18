46681df299625e770d206485212dc2163217b669
Mar-Issah <masy370@gmail.com>
2023-12-18 14:21:41 -0600

    Feat:implement function that update the document

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python3
-""" MongoDB Operations with Python using pymongo """
+""" 10. Change school topics """
 
 
 def update_topics(mongo_collection, name, topics):
-    """ Changes all topics of a school document based on the name """
+    """ Changes all topics of a school doc based on the name
+    Args:
+    mongo_collection: the pymongo collection object
+    name (string): will be the school name to update
+    topics (list of strings): will be the list of topics approached in the school
+    """
     query = {"name": name}
     new_values = {"$set": {"topics": topics}}
 
