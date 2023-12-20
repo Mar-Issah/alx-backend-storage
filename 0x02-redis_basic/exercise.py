176cf9546cc4d6bbe77a07e430a2094c1cdb3427
Mar-Issah <masy370@gmail.com>
2023-12-20 14:45:13 -0600

    Feat: implement methods get and get_int

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -23,30 +23,24 @@ class Cache:
         self._redis.set(random_key, data)
         return random_key
 
-    # def get(self, key: str,
-    #         fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
-    #     '''
-    #         Get data from the cache.
-    #     '''
-    #     value = self._redis.get(key)
-    #     if fn:
-    #         value = fn(value)
-    #     return value
+    def get(self, key: str,
+            fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
+        """Retrieve data from the serevr."""
+        data = self._redis.get(key)
+        if fn:
+            value = fn(data)
+        return value
 
-    # def get_str(self, key: str) -> str:
-    #     '''
-    #         Get a string from the cache.
-    #     '''
-    #     value = self._redis.get(key)
-    #     return value.decode('utf-8')
+    def get_str(self, key: str) -> str:
+        """ Get a string from the server."""
+        data = self._redis.get(key)
+        return data.decode('utf-8')
 
-    # def get_int(self, key: str) -> int:
-    #     '''
-    #         Get an int from the cache.
-    #     '''
-    #     value = self._redis.get(key)
-    #     try:
-    #         value = int(value.decode('utf-8'))
-    #     except Exception:
-    #         value = 0
-    #     return value
+    def get_int(self, key: str) -> int:
+        """Retrieve integer from the server."""
+        data = self._redis.get(key)
+        try:
+            value = int(data.decode('utf-8'))
+        except Exception:
+            value = 0
+        return value
