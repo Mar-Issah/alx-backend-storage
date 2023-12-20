e1359a0460596773f21e4f0c5f430b55d19d8b03
Mar-Issah <masy370@gmail.com>
2023-12-20 14:53:45 -0600

    Feat: implement method count_calls

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -64,8 +64,8 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    # @count_calls
-    # @call_history
+    @count_calls
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
         Store input data in Redis.
